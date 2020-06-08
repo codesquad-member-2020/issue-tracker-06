d23e487e1fdbe86499398175ea6ac35f65ba9365
kiyoesjh <hyeyaa125@gmail.com>
2020-06-08 18:43:47 +0900

    [#1] chore: 개발 환경 구성
    react 환경에 맞춘 webpack, babel, prettier 설정

---
+++ FE/webpack.prod.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/'
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, './.env.production')
+    })
+  ]
+});
