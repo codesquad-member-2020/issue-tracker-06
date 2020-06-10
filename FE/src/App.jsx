cc6743fafa5e066bcd1b037dc9df2152fab3dc65
kiyoesjh <hyeyaa125@gmail.com>
2020-06-10 17:12:01 +0900

    [#3] feat: rout 설정
    - 필요한 페이지에 대한 라우팅 설정

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Main from '@/components/Main/Main';
 import Issue from '@/components/Issue/Issue';
+import IssueDetail from '@/components/IssueDetail/IssueDetail';
+import NewIssue from '@/components/NewIssue/NewIssue';
+import Labels from '@/components/Labels/Labels';
+import Milestones from '@/components/Milestones/Milestones';
+import NewMilestone from '@/components/NewMilestone/NewMilestone';
+
 import GlobalStyleProvider from '@/components/style/GlobalStyle';
 
 const App = () => {
   return (
     <GlobalStyleProvider>
-      <Issue />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/issueList" component={Issue} />
+          <Route path="/issueDetail" component={IssueDetail} />
+          <Route path="/newIssue" component={NewIssue} />
+          <Route path="/labels" component={Labels} />
+          <Route path="/milestones" component={Milestones} />
+          <Route path="/newMilestone" component={NewMilestone} />
+          <Redirect path="*" to="/" />
+        </Switch>
+      </BrowserRouter>
     </GlobalStyleProvider>
   );
 };
