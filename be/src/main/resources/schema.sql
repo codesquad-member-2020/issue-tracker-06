5d4a4105cdf8122065b30bb3e49b88350261273a
beemiel <qnswwrn@gmail.com>
2020-06-11 23:31:54 +0900

    feat: member 테이블 user로 변경

--- be/src/main/resources/schema.sql
+++ be/src/main/resources/schema.sql
@@ -1,9 +1,10 @@
 drop table if exists member;
 
-create table member (
-    member_id int primary key auto_increment,
+create table user (
+    user_id int primary key auto_increment,
     name varchar (255),
-    age int
+    password varchar (255),
+    image varchar (255)
 );
 
 
