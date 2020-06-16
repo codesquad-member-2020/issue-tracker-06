108545c469196b3f979c02fcc08c5f855118087f
beemiel <qnswwrn@gmail.com>
2020-06-16 16:59:49 +0900

    [#23]feat: data.sql 데이터 삽입

--- be/src/main/resources/data.sql
+++ be/src/main/resources/data.sql
@@ -1,3 +1,36 @@
 use issue;
 
-INSERT INTO user (name, password, profile_image) VALUES ('lynn', 'lynn', 'image');
+INSERT INTO user (name, password, profile_image)
+VALUES ('lynn', 'lynn', 'image'),
+       ('ari', 'ari', 'image'),
+       ('joy', 'joy', 'image');
+
+INSERT INTO label (title, background_color, text_color, description)
+VALUES ('BE', '#fcb27e', '#ffffff', '백엔드'),
+       ('FE', '#0a2f6b', '#ffffff', '프론트엔드'),
+       ('feature', '#68ff36', '#ffffff', '기능 추가'),
+       ('enhancement', '#a2eeef', '#ffffff', 'New feature or request');
+
+INSERT INTO milestone (title, due_by, description)
+VALUES ('[BE] 1주차', '2020-06-05', '1주차'),
+       ('[BE] 2주차', '2020-06-12', '2주차'),
+       ('[FE] 1주차', '2020-06-06', '1주차');
+
+INSERT INTO issue (title, content, emoji, is_open, created_at, image, writer, milestone)
+VALUES ('[BE] 프로젝트 세팅', '내용 내용', '이모지..', true, '2020-06-03T14:25:10', '이미지', 1, 1),
+       ('[FE] UI', 'FE 내용 내용', '이모지..', true, '2020-06-02T15:25:10', '이미지', 2, 3);
+
+INSERT INTO comment (content, emoji, created_at, issue, writer)
+VALUES ('뭐야 이거', '이모지..', '2020-06-04T14:25:10', 1, 1),
+       ('확인~~', '이모지..', '2020-06-03T11:25:10', 2, 3);
+
+INSERT INTO issue_label (issue, label)
+VALUES (1,1),
+       (1,3),
+       (2,2),
+       (2,3);
+
+INSERT INTO assignee (issue, user)
+VALUES (1, 1),
+       (2, 2),
+       (2, 3);
--- be/src/main/resources/schema.sql
+++ be/src/main/resources/schema.sql
@@ -1,10 +1,65 @@
+drop table if exists issue_label;
+drop table if exists comment;
+drop table if exists assignee;
+drop table if exists issue;
+drop table if exists label;
+drop table if exists milestone;
 drop table if exists user;
+
+
+create table label (
+    label_id int primary key auto_increment,
+    title varchar (255),
+    background_color varchar (255),
+    text_color varchar (255),
+    description varchar (255)
+);
+
+create table milestone (
+    milestone_id int primary key auto_increment,
+    title varchar (255),
+    due_by date,
+    description varchar (255)
+);
 
 create table user (
     user_id int primary key auto_increment,
     name varchar (255),
     password varchar (255),
     profile_image varchar (255)
+);
+
+create table issue (
+    issue_id int primary key auto_increment,
+    title varchar (255),
+    content text,
+    emoji varchar (255),
+    is_open boolean,
+    created_at datetime,
+    image varchar (255),
+    writer int references user(user_id),
+    milestone int references milestone(milestone_id)
+);
+
+create table issue_label (
+    issue_label_id int primary key auto_increment,
+    issue int references issue(issue_id),
+    label int references label(label_id)
+);
+
+create table assignee (
+    assignee_id int primary key auto_increment,
+    issue int references issue(issue_id),
+    user int references user(user_id)
+);
+
+create table comment (
+    comment_id int primary key auto_increment,
+    content text,
+    emoji varchar (255),
+    created_at datetime,
+    issue int references issue(issue_id),
+    writer int references user(user_id)
 );
 
 
