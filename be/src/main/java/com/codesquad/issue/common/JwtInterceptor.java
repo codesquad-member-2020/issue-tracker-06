00d0aa18de905db75e9456128e9621f11eb96271
beemiel <qnswwrn@gmail.com>
2020-06-23 00:04:06 +0900

    [#8]feat: JwtInterceptor 작성
    컨트롤러로 가기 전 로그인 처리를 하는 인터셉터
    여기서 헤더에 담긴 jwt 토큰을 확인하고 유효성 검사를 한다

---
+++ be/src/main/java/com/codesquad/issue/common/JwtInterceptor.java
@@ -0,0 +1,34 @@
+package com.codesquad.issue.common;
+
+import com.codesquad.issue.domain.JwtProperties;
+import com.codesquad.issue.dto.UserDTO;
+import com.codesquad.issue.exception.UnauthorizedException;
+import com.codesquad.issue.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class JwtInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (request.getMethod().equals("OPTIONS")) {
+            log.info("options 메서드는 통과");
+            log.info("{} : {}", request.getMethod(), request.getServletPath());
+            return true;
+        }
+
+        String jwt = request.getHeader(JwtProperties.HEADER_STRING);
+        if(jwt != null) {
+            UserDTO user = JwtUtil.parseJwt(jwt);
+            request.setAttribute("user", user);
+            return true;
+        }
+
+        throw new UnauthorizedException("로그인이 필요합니다.");
+    }
+
+}
