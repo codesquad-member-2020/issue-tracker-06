697c8b54426360738cf0bcc628fcc14d250451dd
beemiel <qnswwrn@gmail.com>
2020-06-23 00:01:35 +0900

    [#8]feat: loginController에 메소드 추가
    getLoginWIthCode() - github에서 받은 code로 accessToken을 얻고, 그
    토큰으로
    유저 정보를 요청한다.
    받아온 유저 정보로 jwt를 만들고 필요한 정보들과 jwt토큰을 쿠키에 담는다

--- be/src/main/java/com/codesquad/issue/controller/LoginController.java
+++ be/src/main/java/com/codesquad/issue/controller/LoginController.java
@@ -1,16 +1,32 @@
 package com.codesquad.issue.controller;
 
+import com.codesquad.issue.application.LoginService;
 import com.codesquad.issue.domain.GithubProperties;
+import com.codesquad.issue.domain.User;
+import com.codesquad.issue.util.JwtUtil;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/login")
 public class LoginController {
+
+    private LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
 
     @GetMapping
     public void getLogin(HttpServletResponse response) throws IOException {
@@ -19,6 +35,28 @@ public class LoginController {
                 + GithubProperties.REDIRECT_TO
                 + GithubProperties.REDIRECT_URL;
         response.sendRedirect(url);
+    }
+
+    @GetMapping("/github")
+    public ResponseEntity<String> getLoginWithCode(HttpServletResponse response, @RequestParam String code) throws IOException {
+        String accessToken = loginService.getAccessToken(code);
+        User user = loginService.getUserInfo(accessToken);
+
+        loginService.addUser(user);
+        String jwt = JwtUtil.buildJwtToken(user);
+
+        List<Cookie> cookies = Arrays.asList(
+                new Cookie("jwt", jwt),
+                new Cookie("id", user.getName()),
+                new Cookie("profile", user.getProfileImage())
+        );
+
+        for (Cookie cookie: cookies) {
+            cookie.setPath("/");
+            response.addCookie(cookie);
+        }
+        response.sendRedirect(GithubProperties.REDIRECT_TO_MAIN);
+        return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT).body("redirect to main");
     }
 
 }
