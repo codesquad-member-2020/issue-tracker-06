3ddd8bcd22637105f54244cc78d6a001e584c9da
beemiel <qnswwrn@gmail.com>
2020-06-23 03:08:18 +0900

    [#23]feat: IssueService 수정
    새로 추가한 DAO들을 이용해서 추가된 필드들을 세팅하도록 수정

--- be/src/main/java/com/codesquad/issue/application/IssueService.java
+++ be/src/main/java/com/codesquad/issue/application/IssueService.java
@@ -1,23 +1,44 @@
 package com.codesquad.issue.application;
 
 import com.codesquad.issue.domain.IssueDAO;
+import com.codesquad.issue.domain.LabelDAO;
+import com.codesquad.issue.domain.MilestoneDAO;
+import com.codesquad.issue.domain.UserDAO;
 import com.codesquad.issue.dto.IssueOverviewListDTO;
+import com.codesquad.issue.dto.UserDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class IssueService {
 
     private IssueDAO issueDAO;
 
-    public IssueService(IssueDAO issueDAO) {
+    private LabelDAO labelDAO;
+
+    private MilestoneDAO milestoneDAO;
+
+    private UserDAO userDAO;
+
+    public IssueService(IssueDAO issueDAO, LabelDAO labelDAO, MilestoneDAO milestoneDAO, UserDAO userDAO) {
         this.issueDAO = issueDAO;
+        this.labelDAO = labelDAO;
+        this.milestoneDAO = milestoneDAO;
+        this.userDAO = userDAO;
     }
 
     public IssueOverviewListDTO getIssueOverviewListDTO(String is_open, String assignee, String label, String author, String milestone) {
+        List<UserDTO> allUser = userDAO.getAllUser();
+
         return IssueOverviewListDTO.builder()
-                                    .numberOfLabel(issueDAO.getLabelCount())
-                                    .numberOfMilestone(issueDAO.getMilestoneCount())
+                                    .numberOfLabel(labelDAO.getLabelCount())
+                                    .numberOfMilestone(milestoneDAO.getMilestoneCount())
                                     .numberOfIssue(issueDAO.getIssueCount())
+                                    .author(allUser)
+                                    .label(labelDAO.getAllLabel())
+                                    .milestones(milestoneDAO.getAllMilestone())
+                                    .assignee(allUser)
                                     .overviews(issueDAO.getIssuesWithFilter(is_open, assignee, label, author, milestone))
                                     .build();
     }
