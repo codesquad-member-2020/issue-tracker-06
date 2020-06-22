f1cab9144af0910f259cd763490141017375aedd
beemiel <qnswwrn@gmail.com>
2020-06-22 23:51:51 +0900

    [#8]feat: github 상수 추가

--- be/src/main/java/com/codesquad/issue/domain/GithubProperties.java
+++ be/src/main/java/com/codesquad/issue/domain/GithubProperties.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class GithubProperties {
     public static final String CLIENT_ID = "9cc7a6b64f872660d2fd";
+    public static final String CLIENT_SECRETE = "cf999c7f99a15d9775705a0616fc49043c10a892";
     public static final String REDIRECT_URL = "http://localhost:8080/api/login/github";
-    public static final String LOGIN_URL = "https://github.com/login/oauth/authorize?client_id=";
+    public static final String GITHUB_LOGIN_URL = "https://github.com/login/oauth/authorize?client_id=";
+    public static final String GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
+    public static final String GITHUB_USER_INFO_URL = "https://api.github.com/user";
     public static final String REDIRECT_TO = "&redirect_uri=";
     public static final String REDIRECT_TO_MAIN = "http://localhost:8080/api/issues";
 }
