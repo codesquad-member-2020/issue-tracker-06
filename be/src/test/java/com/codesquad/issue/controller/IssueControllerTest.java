22325aaba283a5104292278e76a8e88cfd1e6a05
beemiel <qnswwrn@gmail.com>
2020-06-23 05:22:56 +0900

    [#31]feat: 이슈 추가 테스트 코드 작성

--- be/src/test/java/com/codesquad/issue/controller/IssueControllerTest.java
+++ be/src/test/java/com/codesquad/issue/controller/IssueControllerTest.java
@@ -1,28 +1,35 @@
 package com.codesquad.issue.controller;
 
 import com.codesquad.issue.domain.JwtProperties;
+import com.codesquad.issue.domain.RequestIssue;
 import com.codesquad.issue.dto.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(IssueController.class)
@@ -34,6 +41,9 @@ class IssueControllerTest {
 
     @MockBean
     private IssueController issueController;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     @DisplayName("이슈 DTO 리스트를 반환한다.")
@@ -108,6 +118,40 @@ class IssueControllerTest {
                                 parameterWithName("label").description("label 이름"),
                                 parameterWithName("author").description("author 이름"),
                                 parameterWithName("milestone").description("milestone 이름")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("이슈를 작성한다.")
+    void addIssue() throws Exception {
+        RequestIssue requestIssue = RequestIssue.builder()
+                                                .writer(1)
+                                                .title("test title")
+                                                .content("test content")
+                                                .milestone(1)
+                                                .assignees(Collections.EMPTY_LIST)
+                                                .labels(Arrays.asList(1,2,3))
+                                                .build();
+
+        when(issueController.addIssue(any())).thenReturn(ResponseEntity.ok("성공"));
+
+        mockMvc.perform(post("/api/issues")
+                        .header(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + "jwtToken")
+                        .content(objectMapper.writeValueAsString(requestIssue))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document(
+                        "{class-name}/{method-name}",
+                        preprocessRequest(modifyUris().host("13.209.210.21").port(8080), prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        relaxedRequestFields(
+                                fieldWithPath("writer").description("작성자의 id").type(Integer.class),
+                                fieldWithPath("title").description("이슈 제목").type(String.class),
+                                fieldWithPath("content").description("이슈 내용").type(String.class),
+                                fieldWithPath("milestone").description("선택한 마일스톤").type(Integer.class),
+                                fieldWithPath("assignees[]").description("assignees 리스트").type(ArrayList.class),
+                                fieldWithPath("labels[]").description("label 리스트").type(ArrayList.class)
                         )
                 ));
     }
