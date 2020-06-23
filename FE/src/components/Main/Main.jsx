9288ceed734f369062e34afc27389a67f23aa6c1
랄랄랄 <hyewon3938@naver.com>
2020-06-23 21:00:24 +0900

    [#33] feat : 깃헙로그인 버튼 생성 및 OAuth 연결
    - 메인화면에 깃헙로그인 버튼 생성

--- FE/src/components/Main/Main.jsx
+++ FE/src/components/Main/Main.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
+const Wrap = styled.div`
+  display: flex;
+  justify-content: Center;
+  align-items: center;
+  width: 100%;
+  height: 800px;
+`;
+const LoginButton = styled.button`
+  width: 100px;
+  height: 50px;
+  background: grey;
+  color: white;
+`;
 const Main = () => {
-  return <div>Main</div>;
+  const loginClickHandler = () => {
+    window.location.href = process.env.LOGIN;
+  };
+  return (
+    <Wrap>
+      <LoginButton onClick={loginClickHandler}>깃헙으로 로그인</LoginButton>
+    </Wrap>
+  );
 };
 
 export default Main;
