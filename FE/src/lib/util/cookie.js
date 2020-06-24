bb5d0d360013f18d48c7223982763bc9ed81e37e
랄랄랄 <hyewon3938@naver.com>
2020-06-24 13:56:33 +0900

    [#33] feat : setCookie 키값 변경
    - 서버에서 전달해주는 키값이 변경되어 setCookie의 키값도 변경

--- FE/src/lib/util/cookie.js
+++ FE/src/lib/util/cookie.js
@@ -2,8 +2,9 @@ export const getCookie = (key) => {
   const cookies = {
     jwt:
       'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcm9maWxlX3VybCI6Imh0dHBzOi8vYXZhdGFyczMuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3UvNTgzNTU0OTk_dj00IiwidXNlcl9uYW1lIjoiaHlld29uMzkzOCIsImV4cCI6MTU5MzA4Mjc3M30.zAgZeUwjTGDDxLXko84YXaXYmt7_AcjcjuZo_h7olbQ',
-    profile: 'https://avatars3.githubusercontent.com/u/58355499?v=4',
-    id: 'hyewon3938'
+    user_profile: 'https://avatars3.githubusercontent.com/u/58355499?v=4',
+    user_name: 'hyewon3938',
+    user_id: '1'
   };
 
   return cookies[key];
