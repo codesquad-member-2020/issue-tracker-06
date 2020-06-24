295eadcfb68819f3a6691f0c5177eae2302d4b65
랄랄랄 <hyewon3938@naver.com>
2020-06-25 00:36:37 +0900

    feat : 라벨, 마일스톤 텝 기능 구현
    - 텝 클릭 시 해당 페이지로 이동
    - 해당 페이지일 때 버튼 배경색 변경
    - 텝에 아이콘 추가

--- FE/src/components/LabelsMilestonesTap/LabelsMilestonesTap.jsx
+++ FE/src/components/LabelsMilestonesTap/LabelsMilestonesTap.jsx
@@ -1,28 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
+import LocalOfferOutlinedIcon from '@material-ui/icons/LocalOfferOutlined';
+import EventNoteOutlinedIcon from '@material-ui/icons/EventNoteOutlined';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const LabelsMilestoneWrapper = styled.div`
   display: flex;
 `;
 
 const Tap = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   padding: 6px 14px;
   font-size: 14px;
   font-weight: 600;
   line-height: 20px;
   color: ${({ theme }) => theme.mainFontColor};
   border: 1px solid #e1e4e8;
+  > span {
+    margin-left: 5px;
+  }
   &:hover {
-    background: ${({ theme }) => theme.mainBlueColor};
-    color: white;
+    cursor: pointer;
   }
 `;
 
 const LabelsMilestoneTap = () => {
+  const history = useHistory();
+  const currentLocation = useLocation().pathname;
+  console.log(currentLocation);
+
+  const labelsTapColor =
+    currentLocation === '/labels'
+      ? { background: '#0366D6', color: 'white' }
+      : { background: 'white', color: '#586069' };
+  const milestonesTapColor =
+    currentLocation === '/milestones'
+      ? { background: '#0366D6', color: 'white' }
+      : { background: 'white', color: '#586069' };
+
   return (
     <LabelsMilestoneWrapper>
-      <Tap>Labels</Tap>
-      <Tap>Milestones</Tap>
+      <Tap
+        style={{ background: labelsTapColor.background, color: labelsTapColor.color }}
+        onClick={() => history.push('/labels')}>
+        <LocalOfferOutlinedIcon fontSize="small" />
+        <span>Labels</span>
+      </Tap>
+      <Tap
+        style={{ background: milestonesTapColor.background, color: milestonesTapColor.color }}
+        onClick={() => history.push('/milestones')}>
+        <EventNoteOutlinedIcon fontSize="small" />
+        <span>Milestones</span>
+      </Tap>
     </LabelsMilestoneWrapper>
   );
 };
